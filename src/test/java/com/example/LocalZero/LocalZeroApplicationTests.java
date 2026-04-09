39942e4e3e82fa4cae26e202cca9eb6c0e965a02
Jawadigital <jawadimohammad.jm@gmail.com>
2026-04-09 12:43:53 +0200

    first commit

---
+++ src/test/java/com/example/LocalZero/LocalZeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LocalZero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalZeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
