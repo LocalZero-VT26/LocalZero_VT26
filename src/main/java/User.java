148063f96d27a7284cab96906e7cb0e192daa761
SamuelSchulze05 <samuel.sch05@outlook.com>
2026-04-09 13:31:42 +0200

    Basic User Def

---
+++ src/main/java/User.java
@@ -0,0 +1,24 @@
+import java.util.List;
+
+public class User {
+    private String name;
+    private List[] roles;
+
+    // Constructor
+    public User(String name, List[] roles) {
+        this.name = name;
+        this.roles = roles;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Getter for roles
+    public List[] getRoles() {
+        return roles;
+    }
+
+
+}
