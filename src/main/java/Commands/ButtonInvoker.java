32b285e3065916f2edec2a7762cab873f77dc705
SamuelSchulze05 <samuel.sch05@outlook.com>
2026-04-10 16:22:54 +0200

    Invoker button

--- src/main/java/Commands/ButtonInvoker.java
+++ src/main/java/Commands/ButtonInvoker.java
@@ -1,4 +1,13 @@
 package Commands;
 
 public class ButtonInvoker {
+    private Command command;
+
+    public ButtonInvoker(Command command) {
+        this.command = command;
+    }
+
+    public void press() {
+        command.execute();
+    }
 }
