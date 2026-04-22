cff7b2caf31f9918c74f5ca78488e6f7070cd1f0
Jawadigital <jawadimohammad.jm@gmail.com>
2026-04-22 15:08:00 +0200

    validation exception

---
+++ src/main/java/com/example/LocalZero/exception/ValidationException.java
@@ -0,0 +1,7 @@
+package com.example.LocalZero.exception;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
