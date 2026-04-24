c708b3ee6060e9405314faaf24db6fb8bb5a99c7
AlperEken <alpereken04@gmail.com>
2026-04-24 20:55:48 +0200

    Adding HomeController with welcome message

---
+++ src/main/java/com/example/LocalZero/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.LocalZero.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Välkomna, sidan fungerar som det ska!";
+    }
+}
