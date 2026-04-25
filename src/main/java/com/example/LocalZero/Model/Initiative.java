9b235fac151075c15a028600c9fcc44b61d5c050
AlperEken <alpereken04@gmail.com>
2026-04-25 17:23:17 +0200

    Changes in Initiative-class to JPA entity with 2 new fields

--- src/main/java/com/example/LocalZero/Model/Initiative.java
+++ src/main/java/com/example/LocalZero/Model/Initiative.java
@@ -1,27 +1,46 @@
 package com.example.LocalZero.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "initiatives")
+@Getter
+@Setter
+@NoArgsConstructor
+/*
+The "@NoArgsConstructor" above automatically generates a
+default-constructor for us.
+ */
 public class Initiative {
-    private String title;
-    private String description;
-    private String location;
 
-
-    public Initiative(String title, String description, String location) {
+    public Initiative(String title, String description, String location, String duration, String category){
         this.title = title;
         this.description = description;
         this.location = location;
+        this.duration = duration;
+        this.category = category;
     }
 
-    // Getters
-    public String getTitle() {
-        return title;
-    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    public String getDescription() {
-        return description;
-    }
+    @Column(nullable = false)
+    private String title;
 
-    public String getLocation() {
-        return location;
-    }
+    @Column(nullable = false, length = 1000)
+    private String description;
+
+    @Column (nullable = false)
+    private String location;
+
+    @Column (nullable = false)
+    private String duration;
+
+    @Column (nullable = false)
+    private String category;
 }
