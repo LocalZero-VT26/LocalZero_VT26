34808ee8e4f8b900aa4b0d8e414fa66d921540ba
Jawadigital <jawadimohammad.jm@gmail.com>
2026-04-27 12:08:41 +0200

    repo for blacklistedToken

---
+++ src/main/java/com/example/LocalZero/repository/BlacklistedTokenRepository.java
@@ -0,0 +1,11 @@
+package com.example.LocalZero.repository;
+
+import com.example.LocalZero.Model.BlacklistedToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlacklistedTokenRepository extends JpaRepository<BlacklistedToken, Long> {
+    boolean existsByToken(String token);
+
+}
