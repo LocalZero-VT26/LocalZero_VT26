6d201c64b20d94d5c36bdd1ed7d927f995266c54
Jawadigital <jawadimohammad.jm@gmail.com>
2026-04-27 16:36:22 +0200

    removed unused imports

--- src/main/java/com/example/LocalZero/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/LocalZero/config/JwtAuthenticationFilter.java
@@ -6,15 +6,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.http.HttpHeaders;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.List;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
--- src/main/java/com/example/LocalZero/config/SecurityConfig.java
+++ src/main/java/com/example/LocalZero/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.example.LocalZero.config;
 
-import com.example.LocalZero.config.JwtAuthenticationFilter;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
