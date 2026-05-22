c3b3ae95449442820a5a15d048224fd9b6aaf724
AlperEken <alpereken04@gmail.com>
2026-05-22 22:16:31 +0200

    Implementing projects own exceptions into ContentValidator

--- src/main/java/com/example/LocalZero/service/sustainability/ContentValidator.java
+++ src/main/java/com/example/LocalZero/service/sustainability/ContentValidator.java
@@ -1,13 +1,14 @@
 package com.example.LocalZero.service.sustainability;
 
 import com.example.LocalZero.Model.User;
+import com.example.LocalZero.exception.ValidationException;
 
 public class ContentValidator extends EcoValidator{
 
     @Override
     public void validate(String description, User user) {
         if(description == null || description.trim().isEmpty()) {
-            throw new IllegalArgumentException("Description cannot be empty");
+            throw new ValidationException("Description cannot be empty");
         }
 
         if (user != null) {
