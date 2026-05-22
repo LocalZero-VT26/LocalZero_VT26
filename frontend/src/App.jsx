b5d1a6777b23ef97fded99d27847c1d0be96f637
SamuelSchulze05 <samuel.sch05@outlook.com>
2026-05-22 22:42:13 +0200

    Har lagt till routing för Initiativ sidan

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,20 @@
-import {Route, Routes, Navigate, useLocation} from "react-router-dom";
-import AuthPage from './pages/AuthPage'
-import HomePage from './pages/HomePage'
-import authService from './services/authService'
+import {Routes, Route, Navigate, useLocation} from 'react-router-dom';
+import AuthPage from './pages/AuthPage';
+import HomePage from './pages/HomePage';
+import InitiativePage from './pages/InitiativePage';
+import authService from "./services/authService.js";
 
 function App() {
-  useLocation()
-  const user = authService.getCurrentUser()
+    useLocation()
+    const user = authService.getCurrentUser();
 
-  return (
-      <Routes>
-        <Route path="/" element={user ? <Navigate to="/home" replace /> : <AuthPage />} />
-        <Route path="/home" element={user ? <HomePage/> : <Navigate to="/" replace />} />
-      </Routes>
-  )
+    return (
+        <Routes>
+            <Route path="/" element={user ? <Navigate to="/home" replace /> : <AuthPage />} />
+            <Route path="/home" element={user ? <HomePage/> : <Navigate to="/" replace />} />
+            <Route path="/initiatives" element={<InitiativePage/>} />
+        </Routes>
+    );
 }
 
-
-
-export default App
+export default App;
