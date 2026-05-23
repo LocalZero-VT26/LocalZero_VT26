26c0cca4856eedb35a337a286c45cf1360e4ccad
AlperEken <alpereken04@gmail.com>
2026-05-23 23:26:17 +0200

    Adding query to fetch user eco-actions sorted by timestamp descending

--- src/main/java/com/example/LocalZero/repository/EcoActionRepository.java
+++ src/main/java/com/example/LocalZero/repository/EcoActionRepository.java
@@ -1,9 +1,12 @@
 package com.example.LocalZero.repository;
 
 import com.example.LocalZero.Model.EcoAction;
+import com.example.LocalZero.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface EcoActionRepository extends JpaRepository<EcoAction, Long> {
+    List<EcoAction> findByUserOrderByTimestampDesc (User user);
 }
