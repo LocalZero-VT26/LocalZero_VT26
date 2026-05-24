a27efe63f3c09bb4c2873c35711f1ef47c9e20c0
Jawadigital <jawadimohammad.jm@gmail.com>
2026-05-24 16:51:42 +0200

    added new pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import {Route, Routes, Navigate, useLocation} from "react-router-dom";
 import AuthPage from './pages/AuthPage'
 import HomePage from './pages/HomePage'
+import ChatRoomPage from './pages/ChatRoomPage'
+import ChatPage from './pages/ChatPage'
 import authService from './services/authService'
 
 function App() {
@@ -11,6 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={user ? <Navigate to="/home" replace /> : <AuthPage />} />
         <Route path="/home" element={user ? <HomePage/> : <Navigate to="/" replace />} />
+        <Route path="/chatrooms" element={user ? <ChatRoomPage /> : <Navigate to="/" replace />} />
+        <Route path="/chat/new" element={user ? <ChatPage /> : <Navigate to="/" replace />} />
+        <Route path="/chat/:roomId" element={user ? <ChatPage /> : <Navigate to="/" replace />} />
       </Routes>
   )
 }
