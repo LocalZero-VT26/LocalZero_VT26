3043da8d0e11380a7db631fadb369d7a1f8bdf7e
Josef Zakaria Jehia <josef.zakaria@outlook.com>
2026-05-24 22:28:06 +0200

    Removing messages button and page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,6 @@ import {Routes, Route, Navigate, useLocation} from 'react-router-dom';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
 import InitiativePage from './pages/InitiativePage';
-import ChatRoomPage from './pages/ChatRoomPage';
-import ChatPage from './pages/ChatPage';
 import ProfilePage from './pages/ProfilePage';
 import InboxPage from './pages/InboxPage';
 import authService from './services/authService';
@@ -20,9 +18,6 @@ function App() {
                 path="/initiatives"
                 element={user ? <InitiativePage /> : <Navigate to="/" replace />}
             />
-            <Route path="/chatrooms" element={user ? <ChatRoomPage /> : <Navigate to="/" replace />} />
-            <Route path="/chat/new" element={user ? <ChatPage /> : <Navigate to="/" replace />} />
-            <Route path="/chat/:roomId" element={user ? <ChatPage /> : <Navigate to="/" replace />} />
             <Route path="/profile" element={user ? <ProfilePage/> : <Navigate to="/" replace />} />
             <Route path="/inbox" element={user ? <InboxPage/> : <Navigate to="/" replace />} />
         </Routes>
