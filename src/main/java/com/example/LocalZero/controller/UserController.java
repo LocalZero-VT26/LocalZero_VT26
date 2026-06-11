a968c13c3557c5c540e5eaddfebb5190a65e2153
AlperEken <alpereken04@gmail.com>
2026-06-11 19:05:39 +0200

    Adding assign-role endpoint in UserController

--- src/main/java/com/example/LocalZero/controller/UserController.java
+++ src/main/java/com/example/LocalZero/controller/UserController.java
@@ -5,6 +5,7 @@ import com.example.LocalZero.service.IUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.example.LocalZero.dto.AssignRoleRequest;
 
 import java.util.List;
 
@@ -18,5 +19,12 @@ public class UserController {
     @GetMapping("/available")
     public ResponseEntity<List<UserSummaryResponse>> getAvailableUsers(@RequestAttribute("email") String email) {
         return ResponseEntity.ok(userService.getAvailableUsers(email));
+    }
+
+    @PutMapping("/assign-role")
+    public ResponseEntity<Void> assignrole(@jakarta.validation.Valid @RequestBody AssignRoleRequest request,
+                                           @RequestAttribute("email") String callerEmail){
+        userService.assignRole(request, callerEmail);
+        return ResponseEntity.ok().build();
     }
 }
